11459a91e4be2f7a9ad95fb8a243aeefb2aa9abd
tarodo <rsvolozhanin@gmail.com>
2026-04-14 11:44:18 +0400

    feat: scaffold aide-sdk package with exceptions

---
+++ sdk/aide_sdk/__init__.py
@@ -0,0 +1,15 @@
+from aide_sdk.exceptions import (
+    AideApiError,
+    AuthError,
+    ConflictError,
+    NotFoundError,
+    ValidationError,
+)
+
+__all__ = [
+    "AideApiError",
+    "AuthError",
+    "ConflictError",
+    "NotFoundError",
+    "ValidationError",
+]
